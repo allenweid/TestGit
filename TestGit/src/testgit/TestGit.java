8412bb760d2527ed1b893b59a4c9772cfafc8e82
weid <weid@192.168.95.194>
2015-11-04 12:01:32 +0800

    TestGit项目首次提交代码

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testgit;
 
 /**
- *
+ * git代码管理测试
  * @author weid
  */
 public class TestGit {
 
     /**
-     * @param args the command line arguments
+     * 程序的入口函数
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("测试git服务器的使用");
     }
     
 }
